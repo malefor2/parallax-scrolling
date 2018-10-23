92eaf1e9e7808fa6812c74daad4310ae4b14a083
malefor2 <malefor22@gmail.com>
2018-10-23 14:41:10 +0200

    added bar

--- scroll.js
+++ scroll.js
@@ -2,4 +2,16 @@
 window.addEventListener('scroll', function() {
   var bg = document.getElementById('bg');
   bg.style.transform = "translate(0, " + -pageYOffset/5 + "px" + ")";
+  var bar = document.getElementById('bar')
+  if(document.body.scrollTop > 120 || document.documentElement.scrollTop > 120){
+    bar.classList.remove("bar-static");
+    bar.classList.add("bar-moving");
+  }
+  else{
+    bar.classList.add("bar-static");
+    bar.classList.remove("bar-moving");
+  }
 });
+function moveScroll(scrollPos){
+  document.getElementById("p"+scrollPos).scrollIntoView({behavior: 'smooth', block: 'start'});
+}
